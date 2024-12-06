381a413be7e700d68130ab0c96b2d89aa7bf4bac
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2024-12-06 13:28:00 -0300

    Mudanças no NodeRulesV2

--- gen02/contracts/NodeRulesV2.sol
+++ gen02/contracts/NodeRulesV2.sol
@@ -3,6 +3,9 @@ pragma solidity 0.8.26;
 
 import "./NodeRulesProxy.sol";
 
+bytes32 constant GLOBAL_ADMIN_ROLE = keccak256("GLOBAL_ADMIN_ROLE");
+bytes32 constant LOCAL_ADMIN_ROLE = keccak256("LOCAL_ADMIN_ROLE");
+
 interface NodeRulesV2 is NodeRulesProxy {
 
     enum NodeType{
@@ -21,24 +24,25 @@ interface NodeRulesV2 is NodeRulesProxy {
         string status;
     }
 
-    event NodeAdded(address nodeAddress, address admin);
-    event NodeRemoved(address nodeAddress, address admin);
-    event NodeEdited(address nodeAddress, address admin);
-    event NodeStatusChanged(address nodeAddress, address admin);
-    event NodeAddedByGovernance(address nodeAddress, address governance);
-    event NodeRemovedByGovernance(address nodeAddress, address governance);
+    event NodeAdded(bytes32 enodeHigh, bytes32 enodeLow, address admin);
+    event NodeRemoved(bytes32 enodeHigh, bytes32 enodeLow,address admin);
+    event NodeUpdated(bytes32 enodeHigh, bytes32 enodeLow, address admin);
+    event NodeStatusUpdated(bytes32 enodeHigh, bytes32 enodeLow, address admin);
+    event NodeAddedByGovernance(bytes32 enodeHigh, bytes32 enodeLow, address governance);
+    event NodeRemovedByGovernance(bytes32 enodeHigh, bytes32 enodeLow, address governance);
 
     error InvalidArgument(string message);
     error InactiveAccount(address account, string message);
-    error InvalidOrganization(uint orgId, string message);
-    error NodeAlreadyExists(address nodeAddress, string message);
-    error NodeDoesntExist(string message);
+    error InvalidOrganization(uint orgId);
+    error NodeAlreadyExists(bytes32 enodeHigh, bytes32 enodeLow, string message);
+    error NodeDoesntExist(bytes32 enodeHigh, bytes32 enodeLow, string message);
     error InvalidState(string message);
+    error UnauthorizedAccess(address account);
 
-    function addNode(bytes32 _enodeHigh, bytes32 _enodeLow, NodeType _nodeType, string memory _name, string memory _organization) external;
-    function removeNode(bytes32 _enodeHigh, bytes32 _enodeLow) external;
-    function editNode(bytes32 _enodeHigh, bytes32 _enodeLow, NodeType _nodeType, string memory _name) external;
-    function changeNodeStatus(bytes32 _enodeHigh, bytes32 _enodeLow, string memory status) external;
+    function addNode(bytes32 _enodeHigh, bytes32 _enodeLow, NodeType _nodeType, string memory _name, string memory _organization) external returns (bool);
+    function removeNode(bytes32 _enodeHigh, bytes32 _enodeLow) external returns (bool);
+    function updateNode(bytes32 _enodeHigh, bytes32 _enodeLow, NodeType _nodeType, string memory _name) external returns (bool);
+    function updateNodeStatus(bytes32 _enodeHigh, bytes32 _enodeLow, string memory status) external returns (bool);
 
     /* NOTAS:
     
