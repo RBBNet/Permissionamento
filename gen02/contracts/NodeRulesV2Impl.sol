2550e4af9fccb5b7eb9d2d7160ac51c5197f34e7
Lionel Rocha <111009073+Lionel-Rocha@users.noreply.github.com>
2024-12-09 14:11:13 -0300

    Casos de uso 01, 06, 07 e 08

--- gen02/contracts/NodeRulesV2Impl.sol
+++ gen02/contracts/NodeRulesV2Impl.sol
@@ -4,14 +4,17 @@ pragma solidity 0.8.26;
 import "./NodeRulesV2.sol";
 import "./Governable.sol";
 import "./AccountRulesV2.sol";
+import "./Organization.sol";
 
 contract NodeRulesV2Impl is NodeRulesV2, Governable {
 
     AccountRulesV2 public immutable contractRules;
+    Organization public immutable contractOrganization;
     mapping (uint => NodeData) public allowedNodes;
 
-    constructor(address rulesAddress, AdminProxy adminProxy) Governable(adminProxy) {
-        contractRules = AccountRulesV2(rulesAddress); // Define o endereço do contrato A
+    constructor(address rulesAddress, address organizationAddress, AdminProxy adminProxy) Governable(adminProxy) {
+        contractRules = AccountRulesV2(rulesAddress); // Define o endereço do contrato 
+        contractOrganization = Organization(organizationAddress);
     }
     
     modifier onlyActiveAdmin() {
@@ -24,17 +27,40 @@ contract NodeRulesV2Impl is NodeRulesV2, Governable {
         _;
     }
 
-    function addNode(bytes32 enodeHigh,bytes32 enodeLow,NodeType nodeType, string calldata name, uint organization) public onlyGovernance {
+    //USNOD01 - OK
+    function addNode(bytes32 enodeHigh,bytes32 enodeLow,NodeType nodeType, string calldata name) public onlyActiveAdmin{
         uint256 key = _calculateKey(enodeHigh, enodeLow);
         if (_nodeExists(key)) {
             revert NodeAlreadyExists(enodeHigh, enodeLow, "This node already exists.");
         }
 
+        AccountRulesV2.AccountData memory acc = contractRules.getAccount(msg.sender);
+        uint organization = acc.orgId;
+
         allowedNodes[key] = NodeData(enodeHigh, enodeLow, nodeType, name, organization, true);
         emit NodeAdded(enodeHigh, enodeLow, msg.sender);
     }
 
-    function removeNode(bytes32 enodeHigh, bytes32 enodeLow) public onlyGovernance {
+    //USNOD05 - não sei se vai pegar o erro OrganizationNotFound do Organization, e se essa verificação é suficiente
+    function addNodeByGovernance(bytes32 enodeHigh,bytes32 enodeLow,NodeType nodeType, string calldata name, uint organization) public onlyGovernance {
+        uint256 key = _calculateKey(enodeHigh, enodeLow);
+        if (_nodeExists(key)) {
+            revert NodeAlreadyExists(enodeHigh, enodeLow, "This node already exists.");
+        }
+
+        bool isOrgActive = contractOrganization.isOrganizationActive(organization);
+
+        if (isOrgActive){
+            allowedNodes[key] = NodeData(enodeHigh, enodeLow, nodeType, name, organization, true);
+            emit NodeAdded(enodeHigh, enodeLow, msg.sender);
+        } else {
+            revert InvalidOrganization(organization);
+        }
+        
+    }
+
+    //USNOD06 - OK
+    function removeNodeByGovernance(bytes32 enodeHigh, bytes32 enodeLow) public onlyGovernance {
         uint256 key = _calculateKey(enodeHigh, enodeLow);
         if (!_nodeExists(key)) {
             revert NodeDoesntExist(enodeHigh, enodeLow, "Node does not exist");
@@ -44,31 +70,101 @@ contract NodeRulesV2Impl is NodeRulesV2, Governable {
         emit NodeRemoved(enodeHigh, enodeLow, msg.sender);
     }
     
-    //TODO: verificar validade das novas informações?
+    //USNOD02 - OK
+    function removeNode(bytes32 enodeHigh, bytes32 enodeLow) public onlyActiveAdmin {
+        uint256 key = _calculateKey(enodeHigh, enodeLow);
+        if (!_nodeExists(key)) {
+            revert NodeDoesntExist(enodeHigh, enodeLow, "Node does not exist");
+        }
+
+        NodeData memory nodeA = allowedNodes[key];
+        uint nodeOrg = nodeA.orgId;
+
+        AccountRulesV2.AccountData memory acc = contractRules.getAccount(msg.sender);
+        uint accOrg = acc.orgId;
+
+        if (accOrg != nodeOrg){
+            revert InvalidOrganization(accOrg);
+        } else {
+            delete allowedNodes[key];
+            emit NodeRemoved(enodeHigh, enodeLow, msg.sender);
+        }
+        
+    }
+    
+    //USNOD03 - ok? É suficiente?
     function updateNode(bytes32 enodeHigh, bytes32 enodeLow, NodeType nodeType, string calldata name) public onlyActiveAdmin {
         uint256 key = _calculateKey(enodeHigh, enodeLow);
         if (!_nodeExists(key)) {
             revert NodeDoesntExist(enodeHigh, enodeLow, "Node does not exist");
         }
 
+        NodeData memory nodeA = allowedNodes[key];
+        uint nodeOrg = nodeA.orgId;
+
+        AccountRulesV2.AccountData memory acc = contractRules.getAccount(msg.sender);
+        uint accOrg = acc.orgId;
+
+        if (accOrg != nodeOrg){
+            revert InvalidOrganization(accOrg);
+        } 
+
         if (bytes(name).length > 0) {
             allowedNodes[key].name = name;
         }
-        allowedNodes[key].nodeType = nodeType;
 
-        emit NodeUpdated(enodeHigh, enodeLow, msg.sender);
+        if (isValidNodeType(nodeType)){
+            allowedNodes[key].nodeType = nodeType;
+            emit NodeUpdated(enodeHigh, enodeLow, msg.sender);
+        } else {
+            revert InvalidArgument("Invalid node type.");
+        }
+        
     }
 
+    //USNOD04 - dúvida
     function updateNodeStatus(bytes32 enodeHigh, bytes32 enodeLow, bool status) public onlyActiveAdmin {
         uint256 key = _calculateKey(enodeHigh, enodeLow);
         if (!_nodeExists(key)) {
-            revert NodeDoesntExist(enodeHigh, enodeLow, "Node does not exist");
+            revert NodeDoesntExist(enodeHigh, enodeLow, "Node does not exist.");
         }
 
         allowedNodes[key].status = status;
         emit NodeStatusUpdated(enodeHigh, enodeLow, msg.sender);
     }
 
+    //USNOD07 - OK
+    function isNodeActive(bytes32 enodeHigh, bytes32 enodeLow) public view returns (bool){
+        uint256 key = _calculateKey(enodeHigh, enodeLow);
+        if (!_nodeExists(key)){
+            revert NodeDoesntExist(enodeHigh, enodeLow, "Node does not exist.");
+        }
+
+        NodeData memory node = allowedNodes[key];
+        uint nodeOrg = node.orgId;
+        bool isOrgActive = contractOrganization.isOrganizationActive(nodeOrg);
+        bool nodeStatus = node.status;
+
+        if (isOrgActive && nodeStatus) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    //USNOD08
+    function getNode(bytes32 enodeHigh, bytes32 enodeLow) public view returns (NodeData memory){
+        uint256 key = _calculateKey(enodeHigh, enodeLow);
+
+        if (!_nodeExists(key)){
+            revert NodeDoesntExist(enodeHigh, enodeLow, "Node does not exist.");
+        }
+        
+        return allowedNodes[key];
+    }
+
+    //USNOD09
     function connectionAllowed(
         bytes32 sourceEnodeHigh,
         bytes32 sourceEnodeLow,
@@ -98,9 +194,14 @@ contract NodeRulesV2Impl is NodeRulesV2, Governable {
         bytes32 enodeHigh,
         bytes32 enodeLow
     ) public view returns (bool) {
-        // TODO verificar se o nó está ativo
         uint key = _calculateKey(enodeHigh, enodeLow);
-        return _nodeExists(key);
+        NodeData memory node = allowedNodes[key];
+        if (node.status){
+           return _nodeExists(key); 
+        } else {
+            revert InactiveNode(enodeHigh, enodeLow);
+        }
+        
     }
 
     function _nodeExists(uint nodeKey) private view returns(bool) {
@@ -109,6 +210,10 @@ contract NodeRulesV2Impl is NodeRulesV2, Governable {
 
     function _calculateKey(bytes32 enodeHigh, bytes32 enodeLow) private pure returns(uint) {
         return uint(keccak256(abi.encodePacked(enodeHigh, enodeLow)));
+    }
+
+    function isValidNodeType(NodeType _type) public pure returns (bool) {
+        return _type == NodeType.Boot || _type == NodeType.Validator || _type == NodeType.Writer || _type == NodeType.ObserverBoot;
     }
 
 }
