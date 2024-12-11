04f04ad61b4d522937db28c993b0df17d6307a6c
Joao Alexandre dos Santos Lopes <joao.lopes@bndes.gov.br>
2024-12-11 13:06:50 -0300

    Ajustes de implementação

--- gen02/contracts/NodeRulesV2Impl.sol
+++ gen02/contracts/NodeRulesV2Impl.sol
@@ -31,11 +31,8 @@ contract NodeRulesV2Impl is NodeRulesV2, Governable {
     function addLocalNode(bytes32 enodeHigh, bytes32 enodeLow, NodeType nodeType, string calldata name) public onlyActiveAdmin {
         uint256 key = _calculateKey(enodeHigh, enodeLow);
         _revertIfDuplicateNode(enodeHigh, enodeLow, key);
-
         AccountRulesV2.AccountData memory acc = accountsContract.getAccount(msg.sender);
-        uint organization = acc.orgId;
-
-        allowedNodes[key] = NodeData(enodeHigh, enodeLow, nodeType, name, organization, true);
+        allowedNodes[key] = NodeData(enodeHigh, enodeLow, nodeType, name, acc.orgId, true);
         emit NodeAdded(enodeHigh, enodeLow, msg.sender);
     }
 
@@ -44,14 +41,12 @@ contract NodeRulesV2Impl is NodeRulesV2, Governable {
         uint256 key = _calculateKey(enodeHigh, enodeLow);
         _revertIfDuplicateNode(enodeHigh, enodeLow, key);
 
-        bool isOrgActive = organizationsContract.isOrganizationActive(organization);
-
-        if (isOrgActive){
-            allowedNodes[key] = NodeData(enodeHigh, enodeLow, nodeType, name, organization, true);
-            emit NodeAdded(enodeHigh, enodeLow, msg.sender);
-        } else {
+        if(!organizationsContract.isOrganizationActive(organization)) {
             revert InvalidOrganization(organization);
         }
+
+        allowedNodes[key] = NodeData(enodeHigh, enodeLow, nodeType, name, organization, true);
+        emit NodeAdded(enodeHigh, enodeLow, msg.sender);
     }
 
     //USNOD06 - OK
@@ -66,19 +61,14 @@ contract NodeRulesV2Impl is NodeRulesV2, Governable {
     function deleteLocalNode(bytes32 enodeHigh, bytes32 enodeLow) public onlyActiveAdmin {
         uint256 key = _calculateKey(enodeHigh, enodeLow);
         _revertIfNodeNotFound(enodeHigh, enodeLow, key);
-        NodeData memory nodeA = allowedNodes[key];
-        uint nodeOrg = nodeA.orgId;
 
         AccountRulesV2.AccountData memory acc = accountsContract.getAccount(msg.sender);
-        uint accOrg = acc.orgId;
-
-        if (accOrg != nodeOrg){
-            revert InvalidOrganization(accOrg);
-        } else {
-            delete allowedNodes[key];
-            emit NodeDeleted(enodeHigh, enodeLow, msg.sender);
+        if(acc.orgId != allowedNodes[key].orgId){
+            revert InvalidOrganization(acc.orgId);
         }
         
+        delete allowedNodes[key];
+        emit NodeDeleted(enodeHigh, enodeLow, msg.sender);
     }
     
     //USNOD03 - ok? É suficiente?
@@ -86,27 +76,21 @@ contract NodeRulesV2Impl is NodeRulesV2, Governable {
         uint256 key = _calculateKey(enodeHigh, enodeLow);
         _revertIfNodeNotFound(enodeHigh, enodeLow, key);
 
-        NodeData memory nodeA = allowedNodes[key];
-        uint nodeOrg = nodeA.orgId;
-
         AccountRulesV2.AccountData memory acc = accountsContract.getAccount(msg.sender);
-        uint accOrg = acc.orgId;
-
-        if (accOrg != nodeOrg){
-            revert InvalidOrganization(accOrg);
+        if(acc.orgId != allowedNodes[key].orgId){
+            revert InvalidOrganization(acc.orgId);
         } 
 
-        if (bytes(name).length > 0) {
-            allowedNodes[key].name = name;
-        }
-
-        if (isValidNodeType(nodeType)){
-            allowedNodes[key].nodeType = nodeType;
-            emit NodeUpdated(enodeHigh, enodeLow, msg.sender);
-        } else {
+        if(!isValidNodeType(nodeType)){
             revert InvalidArgument("Invalid node type.");
         }
+
+        if(bytes(name).length > 0) {
+            allowedNodes[key].name = name;
+        }
         
+        allowedNodes[key].nodeType = nodeType;
+        emit NodeUpdated(enodeHigh, enodeLow, msg.sender);
     }
 
     //USNOD04 - dúvida
@@ -120,17 +104,11 @@ contract NodeRulesV2Impl is NodeRulesV2, Governable {
     //USNOD07 - OK
     function isNodeActive(bytes32 enodeHigh, bytes32 enodeLow) public view returns (bool){
         uint256 key = _calculateKey(enodeHigh, enodeLow);
-        _revertIfNodeNotFound(enodeHigh, enodeLow, key);
-        NodeData memory node = allowedNodes[key];
-        uint nodeOrg = node.orgId;
-        bool isOrgActive = organizationsContract.isOrganizationActive(nodeOrg);
-        bool nodeStatus = node.status;
-
-        if (isOrgActive && nodeStatus) {
+        NodeData storage node = allowedNodes[key];
+        if(organizationsContract.isOrganizationActive(node.orgId) && node.status) {
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
     //USNOD08
@@ -151,33 +129,11 @@ contract NodeRulesV2Impl is NodeRulesV2, Governable {
         bytes16,
         uint16
     ) public view returns (bytes32) {
-        if (
-            enodePermitted (
-                sourceEnodeHigh,
-                sourceEnodeLow
-            ) && enodePermitted(
-                destinationEnodeHigh,
-                destinationEnodeLow
-            )
-        ) {
+        if(isNodeActive(sourceEnodeHigh, sourceEnodeLow) && isNodeActive(destinationEnodeHigh, destinationEnodeLow)) {
             return 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
         } else {
             return 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
         }
-    }
-
-    function enodePermitted(
-        bytes32 enodeHigh,
-        bytes32 enodeLow
-    ) public view returns (bool) {
-        uint key = _calculateKey(enodeHigh, enodeLow);
-        NodeData memory node = allowedNodes[key];
-        if (node.status){
-           return _nodeExists(key); 
-        } else {
-            revert InactiveNode(enodeHigh, enodeLow);
-        }
-        
     }
 
     function _revertIfDuplicateNode(bytes32 enodeHigh, bytes32 enodeLow, uint nodeKey) private view {
