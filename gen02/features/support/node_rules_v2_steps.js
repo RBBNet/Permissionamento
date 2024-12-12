d3eaded7270cf5d25a7ebda8e15c95fa29281ad8
Lionel-Rocha <lioli@bndes.gov.br>
2024-12-12 13:43:38 -0300

    Testes do USNOD02

--- gen02/features/support/node_rules_v2_steps.js
+++ gen02/features/support/node_rules_v2_steps.js
@@ -2,11 +2,33 @@ const assert = require('assert');
 const { Given, When, Then } = require('@cucumber/cucumber');
 const hre = require("hardhat");
 const { createOrganization, getBoolean } = require('./setup.js');
-
-async function addLocalNode(signer, enodeHigh, enodeLow, name, type) {
-    await this.nodeRules.connect(signer).addLocalNode(enodeHigh, enodeLow, type, name);
+const web3 = require('web3');
+async function addNode(signer, enodeHigh, enodeLow, name, type) {
+    await this.nodeRules.connect(signer).addNode(enodeHigh, enodeLow, type, name);
     const added = await this.nodeRules.getNode(enodeHigh, enodeLow);
     assert.ok(added[0] === enodeHigh);
+}
+
+//forma mais bonita do que vários ifs
+function typeToNumber(type) {
+    const typeMap = {
+        "Validator": 2,
+        "Boot": 1,
+        "Writer": 3,
+        "ObserverBoot": 5
+    };
+
+    return typeMap[type] !== undefined ? typeMap[type] : null;  // Retorna null ou um valor padrão se o tipo não for encontrado
+}
+
+async function removeNode(signer, enodeHigh, enodeLow) {
+    await this.nodeRules.connect(signer).removeNode(enodeHigh, enodeLow);
+    try {
+        await this.nodeRules.getNode(enodeHigh, enodeLow);
+    } catch(error){
+        assert.ok(error.message.includes(`NodeDoesntExist("${enodeHigh}", "${enodeLow}", "Node does not exist.")`));
+    }
+
 }
 
 function checkErrorMessage(error, expectedMessage) {
@@ -19,18 +41,19 @@ Given("que o contrato de nós está implantado", async function() {
     assert.ok(contractAddress != null);
 });
 
-When(/^o administrador "([^"]*)" informa o enodeHigh "([^"]*)", o enodeLow "([^"]*)", o nome "([^"]*)" e o tipo "([^"]*)" do nó para cadastrá-lo$/, async function (admin, enodeHigh, enodeLow, name, type) {
+When(/^a conta "([^"]*)" informa o endereço "([^"]*)" "([^"]*)", o nome "([^"]*)" e o tipo "([^"]*)" do nó para cadastrá-lo$/, async function (admin, enodeHigh, enodeLow, name, type) {
     const signer = await hre.ethers.getSigner(admin);
+    type = typeToNumber(type);
     try {
-        await addLocalNode.call(this, signer, enodeHigh, enodeLow, name, type);
+        await addNode.call(this, signer, enodeHigh, enodeLow, name, type);
     } catch (e) {
         this.error = e;
     }
 });
 
-Then(/^o evento NodeAdded é emitido para a conta "([^"]*)"$/, async function (admin) {
+Then(/^o evento "([^"]*)" é emitido para a conta "([^"]*)"$/, async function (event, admin) {
     const block = await hre.ethers.provider.getBlockNumber();
-    const events = await this.nodeRules.queryFilter("NodeAdded", 0, block);
+    const events = await this.nodeRules.queryFilter(event, 0, block);
     const eventAdmin = events[0].args[2];
     assert.ok(eventAdmin === admin);
 });
@@ -43,14 +66,15 @@ Then(/^o nó de enodeHigh "([^"]*)" e enodeLow "([^"]*)" tem a mesma organizaç�
     assert.ok(nodeOrg === adminOrg);
 });
 
-When(/^o administrador inativo "([^"]*)" informa o enodeHigh "([^"]*)", o enodeLow "([^"]*)", o nome "([^"]*)" e o tipo "([^"]*)" do nó para cadastrá-lo$/, async function (admin, enodeHigh, enodeLow, name, type) {
-    const signer = await hre.ethers.getSigner(admin);
-    try {
-        await this.nodeRules.connect(signer).addLocalNode(enodeHigh, enodeLow, type, name);
-    } catch (error) {
-        checkErrorMessage(error, `InactiveAccount("${admin}", "The account or the respective organization is not active")`);
-    }
-});
+// When(/^o administrador inativo "([^"]*)" informa o endereço "([^"]*)" mais "([^"]*)", o nome "([^"]*)" e o tipo "([^"]*)" do nó para cadastrá-lo$/, async function (admin, enodeHigh, enodeLow, name, type) {
+//     const signer = await hre.ethers.getSigner(admin);
+//     type = typeToNumber(type);
+//     try {
+//         await this.nodeRules.connect(signer).addNode(enodeHigh, enodeLow, type, name);
+//     } catch (error) {
+//         checkErrorMessage(error, `InactiveAccount("${admin}", "The account or the respective organization is not active")`);
+//     }
+// });
 
 Then(/^o nó de enodeHigh "([^"]*)" e enodeLow "([^"]*)" recebe o erro "([^"]*)"$/, async function (enodeHigh, enodeLow, expectedErrorMessage) {
     try {
@@ -62,4 +86,23 @@ Then(/^o nó de enodeHigh "([^"]*)" e enodeLow "([^"]*)" recebe o erro "([^"]*)"
 
 Then (/^o erro recebido é "([^"]*)"$/, async function (error){
     checkErrorMessage(this.error, error);
+});
+
+When(/^a conta "([^"]*)" informa o endereço "([^"]*)" "([^"]*)" para exclusão$/, async function (admin, enodeHigh, enodeLow) {
+    const signer = await hre.ethers.getSigner(admin);
+    try {
+        await removeNode.call(this, signer, enodeHigh, enodeLow);
+    } catch(error) {
+        this.error = error;
+    }
+});
+When(/^a conta de governança "([^"]*)" informa o enodeHigh "([^"]*)", o enodeLow "([^"]*)", o nome "([^"]*)", a organização "([^"]*)" e o tipo "([^"]*)" do nó para cadastrá-lo$/, async function (admin, enodeHigh, enodeLow, name, org, type) {
+    const signer = await hre.ethers.getSigner(admin);
+    try{
+        await this.nodeRules.connect(signer).addNodeByGovernance(enodeHigh, enodeLow,type,name,org);
+        const added = await this.nodeRules.getNode(enodeHigh, enodeLow);
+        assert.ok(added[0] === enodeHigh);
+    } catch(error) {
+        this.error = error;
+    }
 });
