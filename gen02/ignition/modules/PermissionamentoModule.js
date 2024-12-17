7a5ad2376d88a2125800ded090601bc64b4c8c74
Joao Alexandre dos Santos Lopes <joao.lopes@bndes.gov.br>
2024-12-17 19:37:34 -0300

    Ajustando código para implantação do permissionamento.

--- gen02/ignition/modules/PermissionamentoModule.js
+++ gen02/ignition/modules/PermissionamentoModule.js
@@ -1,12 +1,24 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule('PermissionamentoModule', (m) => {
-    if(m.getParameter('organizations').length != m.getParameter('globalAdmins').length) {
+    const adminAddress = m.getParameter('adminAddress');
+    if(adminAddress.length == 0) {
+        throw new Error('Endereço do contrato de admin indefinido');
+    }
+    const adminContract = m.contractAt('AdminMock', adminAddress);
+
+    const organizations = m.getParameter('organizations');
+    const globalAdmins = m.getParameter('globalAdmins');
+    if(organizations.length != globalAdmins.length) {
         throw new Error('Listas de organizações e contas de administradores globais não têm o mesmo tamanho: ' + organizations.length + ' != ' + globalAdmins.length);
     }
     
-    const organizationsContract = m.contract('OrganizationImpl', [m.getParameter('organizations'), m.getParameter('adminAddress')]);
-    const accountsContract = m.contract('AccountRulesV2Impl', [organizationsContract, m.getParameter('globalAdmins') , m.getParameter('adminAddress')]);
+    const organizationsContract = m.contract('OrganizationImpl', [organizations, adminContract]);
+    const accountsContract = m.contract('AccountRulesV2Impl', [organizationsContract, globalAdmins, adminContract]);
+    const nodesContract = m.contract('NodeRulesV2Impl', [organizationsContract, accountsContract, adminContract]);
+    const governanceContract = m.contract('Governance', [organizationsContract, accountsContract]);
     
-    return [ organizationsContract, accountsContract ];
+    m.call(adminContract, 'addAdmin', [governanceContract]);
+    
+    return [ organizationsContract, accountsContract, nodesContract, governanceContract ];
 });
