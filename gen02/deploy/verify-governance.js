9d8d7fa40ec02680fdc0097b9a2b77eba57b53c9
Joao Alexandre dos Santos Lopes <joao.lopes@bndes.gov.br>
2025-02-12 17:42:41 -0300

    Melhorando verificação da implantação da governança

--- gen02/deploy/verify-governance.js
+++ gen02/deploy/verify-governance.js
@@ -6,6 +6,21 @@ const ADMIN_ABI = [
     'function getAdmins() public view returns (address[])'
 ];
 
+const INGRESS_ABI = [
+    'function getContractAddress(bytes32) public view returns(address)',
+    'function setContractAddress(bytes32, address) public returns (bool)'
+];
+
+const RULES_CONTRACT = '0x72756c6573000000000000000000000000000000000000000000000000000000';
+const NODE_INGRESS_ADDRESS = '0x0000000000000000000000000000000000009999';
+const ACCOUNT_INGRESS_ADDRESS = '0x0000000000000000000000000000000000008888';
+
+async function migrate(ingressAddress) {
+    const ingressContract = await hre.ethers.getContractAt(INGRESS_ABI, ingressAddress);
+    const resp = await ingressContract.setContractAddress(RULES_CONTRACT, '0x0000000000000000000000000000000000000001');
+    await resp.wait();
+}
+
 async function verifyGovernance(parameters) {
     await diagnostics();
 
@@ -21,6 +36,26 @@ async function verifyGovernance(parameters) {
     assert.equal(allAdmins[0], governanceAddress, `Administrador master configurado não é o smart contract de governança: ${allAdmins[0]}`);
 
     console.log(`Smart contract de governança ${governanceAddress} corretamente configurado como único administrador master.`);
+    console.log();
+    
+    console.log('--------------------------------------------------');
+    console.log('Tentando realizar reponteiramentos');
+    try {
+        await migrate(NODE_INGRESS_ADDRESS);
+        console.error(' - Opa! Conseguir reponteirar o NodeIngress!');
+    }
+    catch(error) {
+        console.log(` - NodeIngress não pôde ser reponteirado, corretamente: ${error.message}`);
+        console.log('   Tudo certo!');
+    }
+    try {
+        await migrate(ACCOUNT_INGRESS_ADDRESS);
+        console.error(' - Opa! Conseguir reponteirar o AccountIngress!');
+    }
+    catch(error) {
+        console.log(` - AccountIngress não pôde ser reponteirado, corretamente: ${error.message}`);
+        console.log('   Tudo certo!');
+    }
 }
 
 const parameters = getParameters();
