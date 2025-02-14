fa73eec5f24ace9a0d545435736cc2bf83fb7670
Joao Alexandre dos Santos Lopes <joao.lopes@bndes.gov.br>
2025-02-14 15:03:40 -0300

    Refatoração da biblioteca de paginação

--- gen02/contracts/Pagination.sol
+++ gen02/contracts/Pagination.sol
@@ -11,20 +11,7 @@ library Pagination {
     error InvalidPaginationParameter(string message);
 
     function getUintPage(EnumerableSet.UintSet storage set, uint pageNumber, uint pageSize) internal view returns(uint[] memory) {
-        if(pageNumber < 1) {
-            revert InvalidPaginationParameter("Page must be greater or equal to 1 ");
-        }
-        if(pageSize < 1) {
-            revert InvalidPaginationParameter("Page size must be greater or equal to 1 ");
-        }
-        uint start = (pageNumber - 1) * pageSize;
-        if(start > set.length()) {
-            start = set.length();
-        }
-        uint stop = start + pageSize;
-        if(stop > set.length()) {
-            stop = set.length();
-        }
+        (uint start, uint stop) = getPageBounds(set.length(), pageNumber, pageSize);
         uint[] memory page = new uint[](stop - start);
         for(uint i = start; i < stop; ++i) {
             page[i - start] = set.at(i);
@@ -33,6 +20,15 @@ library Pagination {
     }
 
     function getAddressPage(EnumerableSet.AddressSet storage set, uint pageNumber, uint pageSize) internal view returns(address[] memory) {
+        (uint start, uint stop) = getPageBounds(set.length(), pageNumber, pageSize);
+        address[] memory page = new address[](stop - start);
+        for(uint i = start; i < stop; ++i) {
+            page[i - start] = set.at(i);
+        }
+        return page;
+    }
+
+    function getPageBounds(uint length, uint pageNumber, uint pageSize) internal pure returns (uint, uint) {
         if(pageNumber < 1) {
             revert InvalidPaginationParameter("Page must be greater or equal to 1 ");
         }
@@ -40,18 +36,14 @@ library Pagination {
             revert InvalidPaginationParameter("Page size must be greater or equal to 1 ");
         }
         uint start = (pageNumber - 1) * pageSize;
-        if(start > set.length()) {
-            start = set.length();
+        if(start > length) {
+            start = length;
         }
         uint stop = start + pageSize;
-        if(stop > set.length()) {
-            stop = set.length();
+        if(stop > length) {
+            stop = length;
         }
-        address[] memory page = new address[](stop - start);
-        for(uint i = start; i < stop; ++i) {
-            page[i - start] = set.at(i);
-        }
-        return page;
+        return (start, stop);
     }
 
 }
