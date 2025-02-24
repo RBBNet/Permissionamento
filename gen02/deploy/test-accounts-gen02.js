0577cb2936f7df112b84a9f57c321df0a53a724e
Joao Alexandre dos Santos Lopes <joao.lopes@bndes.gov.br>
2025-02-24 18:29:22 -0300

    Ajustes no script de teste de contas

--- gen02/deploy/test-accounts-gen02.js
+++ gen02/deploy/test-accounts-gen02.js
@@ -1,7 +1,7 @@
 const hre = require('hardhat');
 const assert = require('assert');
 const { getParameters, getParameter, diagnostics, askConfirmation, getDefaultSigner, getRoleId } = require('./util.js');
-const { STATUS_ACTIVE, ZEROED_BYTES, NON_ZEROED_ADDRESS } = require('./constants.js');
+const { ZEROED_BYTES, NON_ZEROED_ADDRESS } = require('./constants.js');
 
 async function testAccounts(parameters) {
     await diagnostics();
@@ -11,12 +11,12 @@ async function testAccounts(parameters) {
 
     const accountRulesV2Address = getParameter(parameters, 'accountRulesV2Address');
     const accountsContract = await hre.ethers.getContractAt('AccountRulesV2Impl', accountRulesV2Address);
-
     const accounts = getParameter(parameters, 'accounts');
     for(acc of accounts) {
         try {
             const allowed = await accountsContract.transactionAllowed(acc.account, NON_ZEROED_ADDRESS, 0, 0, 0, ZEROED_BYTES);
-            console.log(` - ${acc.account}: ${allowed ? 'OK' : 'ERRO'}`);
+            const result = allowed ? 'OK' : 'ERRO';
+            console.log(` - ${result} - ${acc.account}`);
         }
         catch(error) {
             console.log(` - Erro na verificação de acesso da conta ${acc.account}: ${error.message}`);
