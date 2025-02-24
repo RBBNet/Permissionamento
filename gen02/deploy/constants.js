3f3f0a03075b4720999c41c4c8d6ac05827b0629
Joao Alexandre dos Santos Lopes <joao.lopes@bndes.gov.br>
2025-02-24 20:01:28 -0300

    Correção e adição de novas constantes

--- gen02/deploy/constants.js
+++ gen02/deploy/constants.js
@@ -1,29 +1,26 @@
 const SET_CONTRACT_ADDRESS_FUNCTION = 'setContractAddress(bytes32,address)';
 const ADD_ADMIN_FUNC = 'addAdmin(address)';
 const REMOVE_ADMIN_FUNC = 'removeAdmin(address)';
-
+const ADD_ACCOUNT_FUNC = 'addAccount(address,uint,bytes32,bytes32)';
 const ADMIN_ABI = [
     'function addAdmin(address) public returns (bool)',
     'function removeAdmin(address) public returns (bool)',
     'function isAuthorized(address) public view returns (bool)',
     'function getAdmins() public view returns (address[])'
 ];
-
 const INGRESS_ABI = [
     'function getContractAddress(bytes32) public view returns(address)',
     'function setContractAddress(bytes32, address) public returns (bool)'
 ];
-
 const NODE_INGRESS_ABI = INGRESS_ABI.concat('function connectionAllowed(bytes32, bytes32, bytes16, uint16, bytes32, bytes32, bytes16, uint16) public view returns (bytes32)');
-
 const ACCOUNT_INGRESS_ABI = INGRESS_ABI.concat('function transactionAllowed(address, address, uint256, uint256, uint256, bytes) public view returns (bool)');
-
-const STATUS_ACTIVE = 1;
-const STATUS_EXECUTED = 4;
+const STATUS_ACTIVE = 0;
+const STATUS_EXECUTED = 3;
 const RESULT_UNDEFINED = 0;
 const RESULT_APPROVED = 1;
 const ZEROED_BYTES = '0x00';
 const ZEROED_BYTES_16 = '0x00000000000000000000000000000000';
+const ZEROED_BYTES_32 = '0x0000000000000000000000000000000000000000000000000000000000000000';
 const RULES_CONTRACT = '0x72756c6573000000000000000000000000000000000000000000000000000000';
 const NON_ZEROED_ADDRESS = '0x0000000000000000000000000000000000000001';
 const NODE_INGRESS_ADDRESS = '0x0000000000000000000000000000000000009999';
@@ -40,6 +37,7 @@ module.exports = {
     SET_CONTRACT_ADDRESS_FUNCTION: SET_CONTRACT_ADDRESS_FUNCTION,
     ADD_ADMIN_FUNC: ADD_ADMIN_FUNC,
     REMOVE_ADMIN_FUNC: REMOVE_ADMIN_FUNC,
+    ADD_ACCOUNT_FUNC,
     ADMIN_ABI: ADMIN_ABI,
     INGRESS_ABI: INGRESS_ABI,
     NODE_INGRESS_ABI: NODE_INGRESS_ABI,
@@ -50,6 +48,7 @@ module.exports = {
     RESULT_APPROVED: RESULT_APPROVED,
     ZEROED_BYTES: ZEROED_BYTES,
     ZEROED_BYTES_16: ZEROED_BYTES_16,
+    ZEROED_BYTES_32: ZEROED_BYTES_32,
     RULES_CONTRACT: RULES_CONTRACT,
     NON_ZEROED_ADDRESS: NON_ZEROED_ADDRESS,
     NODE_INGRESS_ADDRESS: NODE_INGRESS_ADDRESS,
